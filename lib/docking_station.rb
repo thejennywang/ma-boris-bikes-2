fed14441c913251a9a7960d732db3c768bb8f166
thejennywang <thejennywang@gmail.com>
2014-07-08 14:12:31 +0100

    Jenny & Charlotte | Completed docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,35 +1,11 @@
+require_relative 'bike_container'
+
 class Station
 
-	DEFAULT_CAPACITY = 20
-	
-	def initialize (capacity = DEFAULT_CAPACITY)
-		@bikes = []
-		@capacity = capacity
-	end
+	include BikeContainer
 
-	def bikes
-		@bikes
-	end
-
-	def capacity
-		@capacity
-	end
-
-	def dock (bike)
-		raise "Sation is full" if full?
-		@bikes << bike
-	end
-
-	def release(bike)
-		@bikes.delete(bike)
-	end
-
-	def full?
-		bikes.count == capacity
-	end
-
-	def available_bikes
-		bikes.reject {|bike| bike.broken?}
+		def initialize (options = {})
+		self.capacity = options.fetch(:capacity, capacity)
 	end
 
 end
